cea327dcd2d143814c85efc2a3eb0ce98b102fea
Neha <90282970+nehaphuloria@users.noreply.github.com>
2022-05-06 17:11:45 -0400

    Add files via upload

--- data/pharmacies.js
+++ data/pharmacies.js
@@ -21,14 +21,14 @@ async function get(id) {
 }
 
 async function getZip(zid) {
-    if (zid.trim().length === 0) throw 'Id cannot be an empty string or just spaces';
-    if (!zid) throw 'Please provide id';
+    if (zid.trim().length === 0) throw 'zip code cannot be an empty string or just spaces';
+    if (!zid) throw 'Please provide zip code';
     zid=zid.trim();
     //if (!ObjectId.isValid(zid)) throw 'invalid object ID';
-    if (typeof zid !== 'string') throw 'Id must be a string';
+    if (typeof zid !== 'string') throw 'zip code must be a string';
     const pharmacyCollection = await pharmacy();
     const pharmacyNew = await pharmacyCollection.find({"address.zip":zid}).toArray();
-    if (pharmacyNew === null) throw 'No pharmacy with that zid';
+    if (pharmacyNew === null) throw 'No pharmacy found in that zip code';
     return pharmacyNew;  
   }
 
