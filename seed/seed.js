1900124a4ab19098fdea9742ff06cec47c4df400
JunkyPlatipuss <48295292+JunkyPlatipuss@users.noreply.github.com>
2022-05-07 20:36:44 -0400

    Update seed.js

--- seed/seed.js
+++ seed/seed.js
@@ -17,64 +17,6 @@ const main = async () => {
   let doctor8 = undefined;
   let doctor9 = undefined;
   let doctor10 = undefined;
-
-  // try {
-  //   doctor1 = await doctors.create("Krithika", "Gandlaur" , "Female" , "kgm@stevens.edu" , "2016758976", "232 Hancock" , "Jersey City" , "NJ", "07307", "MBBS" , "Dentist", ["Aetna", "Tata", "Reliance"]);
-  //   console.log("doctor details inserted succesfully");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   doctor1 = await doctors.get(doctor1._id);
-  //   console.log(doctor1);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-  // try {
-  //   doctor2 = await doctors.create("Nikhil", "Bhoneja" , "Male" , "nikhilb@stevens.edu" , "2078958976", "232 Grand st" , "Hoboken" , "NJ", "07030", "MBBS" , "Gupt", ["Aetna", "Idea", "Reliance"]);
-  //   console.log("doctors details inserted successfully..");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   doctor2 = await doctors.get(doctor2._id);
-  //   console.log(doctor2);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   let all = await doctors.getAll();
-  //   console.log(all);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   doctor3 = await doctors.create("James", "Bond" , "Male" , "james@nhu.in" , "9167868976", "456 Hacensek" , "Edison" , "NJ", "07080", "BDS MBBS" , "Orthodontist", ["Tata", "Reliance"]);
-  //   console.log("doctor details inserted succesfully");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   doctor4 = await doctors.create("Preet", "Kumar" , "Male" , "pkumar@max.in" , "2015674325", "2 Bergaline ave" , "Union City" , "NJ", "071456", "BDS" , "Orthodpedic", ["Aetna","apollo","LIC","Tata", "Reliance"]);
-  //   console.log("doctor details inserted succesfully");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  
-  // try {
-  //   user1 = await users.createUsers("Preet", "Kumar" ,"11/30/2021" ,"2 Bergaline ave" , "Union City" , "NJ", "07145","2015674325","pkumar@max.in","usernae12","dyfu658");
-  //   console.log("doctor details inserted succesfully");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // try {
-  //   let r1 = await reviews.createReview("627568d7017fd5ac8b3bd401","he is best",2);
-  //   console.log("doctor details inserted succesfully");
-  // } catch (error) {
-  //   console.log(error);
-  // }
-//};
 try {
     doctor1 = await doctors.create("Krithika", "Gandlaur" , "232 Hancock Ave" , "Jersey City" , "NJ", "07307","3456789087", "Orthopedics","True");
     console.log("doctor 1 details inserted succesfully");
